f293a04dfc389f8aca6d072dc9cb7b2b7abd1a40
Sascha Swiercy <sascha.swiercy@googlemail.com>
2019-12-28 17:29:25 +0100

    Add tooltip to labels

--- main.js
+++ main.js
@@ -3,22 +3,25 @@ window.onload = function() {
 	const labelData = {
 		"c-language": {
 			name: "C",
-			href: "https://en.wikipedia.org/wiki/C_(programming_language)"
+			title: "Written in the C programming language"
 		},
 		"extension": {
-			name: "Extension"
+			name: "Extension",
+			title: "Provides extended functionality"
 		},
 		"game-maker": {
 			name: "Game Maker",
-			href: "https://www.yoyogames.com/gamemaker"
+			href: "https://www.yoyogames.com/gamemaker",
+			title: "Developed using the Game Maker by YoYo Games"
 		},
 		"javascript": {
 			name: "JavaScript",
-			href: "https://en.wikipedia.org/wiki/JavaScript"
+			title: "Written in JavaScript"
 		},
 		"sdl": {
 			name: "SDL",
-			href: "https://www.libsdl.org/"
+			href: "https://www.libsdl.org/",
+			title: "Developed using the Simple DirectMedia Layer"
 		}
 	};
 	
@@ -27,15 +30,18 @@ window.onload = function() {
 		function(element) {
 			element.className.split(' ').forEach(
 				function(name) {
-					if (labelData[name]) {
-						if (labelData[name].href) {
-							const link = document.createElement("a");
-							link.href = labelData[name].href;
-							link.innerText = labelData[name].name;
-							element.appendChild(link);
-						} else {
-							element.appendChild(document.createTextNode(labelData[name].name));
+					const data = labelData[name];
+					
+					if (data) {
+						const link = document.createElement("a");
+						link.innerText = data.name;
+						
+						if (data.href) {
+							link.href = data.href;
 						}
+						
+						link.title = data.title;
+						element.appendChild(link);
 					}
 				}
 			);
